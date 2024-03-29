039a029ce7f5fb249062a329e101155d891b8fda
Wattebled Thomas <thomas.wattebled2.etu@univ-lille.fr>
2024-03-29 10:48:23 +0100

    debut projet

---
+++ Projet3_CAR/src/main/java/com/example/Projet3_CAR/Projet3CarApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projet3_CAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet3CarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet3CarApplication.class, args);
+	}
+
+}
---
+++ Projet3_CAR/src/test/java/com/example/Projet3_CAR/Projet3CarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projet3_CAR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet3CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
